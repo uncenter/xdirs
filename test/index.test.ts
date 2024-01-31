b70690b211010a7dae9026c119e97f3177dd5e10
uncenter <47499684+uncenter@users.noreply.github.com>
2024-01-31 17:18:28 -0500

    test: linux tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,6 +7,14 @@ import { dirs } from '../src/index';
 const USERNAME = os.userInfo().username;
 const TEST_DIR = 'MyApp';
 
+const LINUX_DIRS = {
+	data: ``,
+	config: ``,
+	cache: ``,
+	log: ``,
+	temp: ``,
+};
+
 const MACOS_DIRS = {
 	data: `/Users/${USERNAME}/Library/Application Support/${TEST_DIR}`,
 	config: `/Users/${USERNAME}/Library/Preferences/${TEST_DIR}`,
@@ -20,6 +28,43 @@ const WINDOWS_DIRS = {
 	cache: `/Users/${USERNAME}/Library/Caches/${TEST_DIR}`,
 	log: `/Users/${USERNAME}/Library/Logs/${TEST_DIR}`,
 };
+
+if (process.platform === 'linux')
+	describe('linux', () => {
+		test('should use XDG specification', () => {
+			process.env['XDG_DATA_HOME'] = '';
+			process.env['XDG_CONFIG_HOME'] = '';
+			process.env['XDG_CACHE_HOME'] = '';
+			process.env['XDG_STATE_HOME'] = '';
+
+			const paths = dirs(TEST_DIR);
+
+			expect(paths.data).toBe(LINUX_DIRS.data);
+			expect(paths.config).toBe(LINUX_DIRS.config);
+			expect(paths.cache).toBe(LINUX_DIRS.cache);
+			expect(paths.log).toBe(LINUX_DIRS.log);
+			expect(
+				paths.temp.startsWith(`/var/folders/`) && paths.temp.endsWith(`/${TEST_DIR}`),
+			).toBe(true);
+		});
+
+		test('should respect user-defined XDG_*', () => {
+			process.env['XDG_DATA_HOME'] = '/Users/USERNAME/.local/share';
+			process.env['XDG_CONFIG_HOME'] = '/Users/USERNAME/.config';
+			process.env['XDG_CACHE_HOME'] = '/Users/USERNAME/.cache';
+			process.env['XDG_STATE_HOME'] = '/Users/USERNAME/.local/state';
+
+			const paths = dirs(TEST_DIR);
+
+			expect(paths.data).toBe(`/Users/USERNAME/.local/share/${TEST_DIR}`);
+			expect(paths.config).toBe(`/Users/USERNAME/.config/${TEST_DIR}`);
+			expect(paths.cache).toBe(`/Users/USERNAME/.cache/${TEST_DIR}`);
+			expect(paths.log).toBe(`/Users/USERNAME/.local/state/${TEST_DIR}`);
+			expect(
+				paths.temp.startsWith(`/var/folders/`) && paths.temp.endsWith(`/${TEST_DIR}`),
+			).toBe(true);
+		});
+	});
 
 if (process.platform === 'darwin')
 	describe('macos', () => {
