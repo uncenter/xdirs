3f89dde44d487c839fcc587e99c3811cb8b39e90
uncenter <47499684+uncenter@users.noreply.github.com>
2024-02-01 09:55:17 -0500

    test: remove console logs

--- test/index.test.ts
+++ test/index.test.ts
@@ -38,7 +38,6 @@ if (process.platform === 'linux')
 			process.env['XDG_STATE_HOME'] = '';
 
 			const paths = dirs(TEST_DIR);
-			console.log({ paths });
 
 			expect(paths.data).toBe(LINUX_DIRS.data);
 			expect(paths.config).toBe(LINUX_DIRS.config);
@@ -93,7 +92,6 @@ if (process.platform === 'darwin')
 			process.env['XDG_STATE_HOME'] = '/Users/USERNAME/.local/state';
 
 			const paths = dirs(TEST_DIR);
-			console.log({ paths });
 
 			expect(paths.data).toBe(`/Users/USERNAME/.local/share/${TEST_DIR}`);
 			expect(paths.config).toBe(`/Users/USERNAME/.config/${TEST_DIR}`);
@@ -131,7 +129,6 @@ if (process.platform === 'win32')
 			process.env['XDG_STATE_HOME'] = '';
 
 			const paths = dirs(TEST_DIR);
-			console.log({ paths });
 
 			expect(paths.data).toBe(WINDOWS_DIRS.data);
 			expect(paths.config).toBe(WINDOWS_DIRS.config);
